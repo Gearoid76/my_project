dcf0204412bb375c0244a07f8b9303461da23dfd
Gearóid O'Ceallacháin <gocealla@gmail.com>
2022-09-29 11:15:03 +0300

    files2 and files3

--- file2.py
+++ file2.py
@@ -1,2 +1,3 @@
 """this is a simple program file2."""
 print("hello file2")
+print("this is apart of file3")
--- file3.py
+++ file3.py
@@ -1,2 +1,3 @@
 """this is a simple program."""
 print("hello file3")
+print("this is apart of file2")
